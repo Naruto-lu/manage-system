d3b09609f7cd6c05ac56e2ffaea8d36adc878909
Naruto-lu <15188556790@163.com>
2017-11-10 23:31:01 +0800

    update

--- src/components/page/photo/reduce.vue
+++ src/components/page/photo/reduce.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="photoreduce">
-    <div class="blogmanage-crumbs">
+    <div class="photoreduce-crumbs">
       <el-breadcrumb separator="/">
         <el-breadcrumb-item><i class="el-icon-delete"></i> 照片回收站</el-breadcrumb-item>
       </el-breadcrumb>
@@ -10,7 +10,7 @@
 
 <style lang="less">
   .photoreduce{
-    .blogmanage-crumbs{
+    .photoreduce-crumbs{
       padding: 5px;
       background-color: #ede7ed;
       margin-bottom: 5px;
